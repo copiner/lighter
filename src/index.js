d94b8d79b6e4d95617bb1128fc981ea0ba1c0add
copiner <wdaonngg@gmail.com>
2019-10-11 11:07:56 +0800

    conf

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+
 import React from 'react';
 import ReactDom from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducer from './reducers/index.js';
 import App from './containers/App.js';
+
+import "./index.scss"
 
 let store = createStore(reducer);
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,31 @@
 const webpack = require('webpack');
 const path = require("path");
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: "development", // "production" | "development" | "none"
   devtool: 'eval-source-map', //source maps
-  entry:  __dirname + "/src/index.js",
+  //entry:  __dirname + "/src/index.js",
+  entry: {
+     //vendor: ['helper', 'lib'],
+     app: './src/index.js'
+  },
   output: {
     path: __dirname + "/build",
-    filename: "bundle-[hash].js"
+    //filename: "bundle-[hash].js"
+    filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "build"),
-    historyApiFallback: true,
+    //historyApiFallback: true,
+    host:"127.0.0.1",
+    port: 9000,
+    compress: true,
     inline: true,
-    hot: true,
+    hot: true
   },
   module: {
         rules: [
@@ -27,18 +37,6 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
-               test: /\.(png|jpg|gif)$/,
-               use: [
-                 {
-                   loader: 'url-loader',
-                   options: {
-                     limit: 8192
-                     //fallback: 'file-loader'//If the file is greater than the limit (in bytes) the file-loader is used by default
-                   }
-                 }
-               ]
-            },
-            {
               test: /\.(png|jpg|gif)$/,
               use: [
                 {
@@ -46,28 +44,64 @@ module.exports = {
                   options: {
                     name: '[path][name].[ext]'
                   }
+                },
+                {
+                  loader: 'url-loader',
+                  options: {
+                    limit: 8192
+                    //fallback: 'file-loader'//If the file is greater than the limit (in bytes) the file-loader is used by default
+                  }
                 }
               ]
             },
             {
-                test: /\.css$/,
+                test: /\.(sa|sc|c)ss$/,
                 use: [
-                    { loader: "style-loader" },
+                    "style-loader",
                     {
-                      loader: "css-loader",
-                      options: {
-                            modules: true
-                        }
-                    }
+                      loader: MiniCssExtractPlugin.loader,
+                       options: {
+                         // you can specify a publicPath here
+                         // by default it uses publicPath in webpackOptions.output
+                         //publicPath: '../',
+                         // only enable hot in development
+                         hmr: process.env.NODE_ENV === 'development',
+                       },
+                   },
+                   'css-loader',
+                   // Compiles Sass to CSS
+                   'sass-loader'
                 ]
             }
         ]
     },
     plugins: [
-         new HtmlWebpackPlugin({
+         new HtmlPlugin({
             template: __dirname + "/src/index.html"
          }),
-         new webpack.NamedModulesPlugin(),
-         new webpack.HotModuleReplacementPlugin()
-     ]
+         //new webpack.NamedModulesPlugin(),
+         //new webpack.HotModuleReplacementPlugin(),
+
+         new CompressionPlugin(),
+         new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            //filename: '[name].css',
+            //chunkFilename: '[id].css',
+            filename:"index.css",
+
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+          })
+     ],
+     optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+              name: 'vendor',
+              chunks: 'all',
+            }
+          }
+        }
+      }
 }
